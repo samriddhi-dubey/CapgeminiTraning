68643808b6c106856d7c095d704cd57e9f1de6b6
Samriddhi Dubey <samriddhidubey798@gmail.com>
2025-03-29 14:01:12 +0530

    message

---
+++ src/taskProgramInterface/Vehicle.java
@@ -0,0 +1,18 @@
+package taskProgramInterface;
+
+public interface Vehicle {
+	
+	void start();
+	void stop();
+    
+    
+    void accelerate(int increment);
+    void brake(int decrement);
+    void getCurrentSpeed();
+    void displayType();
+    
+    	
+    
+    
+
+}
